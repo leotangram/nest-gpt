e8da6a94391bafe5f64ba468275ab10c5a3b09f8
Leonardo Omaña <leotangram@gmail.com>
2024-01-19 15:49:58 -0500

    open ai - config account & token

--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -5,5 +5,5 @@ interface Options {
 export const orthographyCheckUseCase = async (options: Options) => {
   const { prompt } = options;
 
-  return { prompt };
+  return { prompt, apiKey: process.env.OPEN_AI_API_KEY };
 };
